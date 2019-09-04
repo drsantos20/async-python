25d3c20c5843515bc14babea0c86068bcd491804
Daniel Ribeiro dos Santos <drsantos20@gmail.com>
2019-09-03 23:53:10 -0300

    Add coroutines async event loop

--- theory.py
+++ theory.py
@@ -6,17 +6,28 @@ def odds(start, stop):
     for odd in range(start, stop + 1, 2):
         yield odd
 
-def random():
-    time.sleep(3)
+# coroutines
+async def random():
+    await asyncio.sleep(3)
     return randint(1, 10)
 
-def main():
+async def main():
     odd_values = [odd for odd in odds(3, 15)]
 
-    #generators to -> tuple (under the hood)
+    # generators to -> tuple (under the hood)
     odds2 = tuple(odds(21, 29))
     print(odd_values)
     print(odds2)
 
+    start = time.perf_counter()
+    r = await random()
+    elapsed = time.perf_counter() - start
+    print(f'{r} took {elapsed:0.2f} seconds.')
+
+    start = time.perf_counter()
+    r = await asyncio.gather(*(random() for _ in range(10)))
+    elapsed = time.perf_counter() - start    
+    print(f'{r} took {elapsed:0.2f} seconds.')
+
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
